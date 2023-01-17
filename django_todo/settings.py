87c2f90162c5da3937f85fd3aa8d59901093fbd9
KevJohBer <kevin.bergstrom0313@gmail.com>
2023-01-17 07:03:54 +0000

    setup development environment

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -15,7 +15,7 @@ import os
 import dj_database_url
 
 
-#   development = os.environ.get('DEVELOPMENT', False)
+development = os.environ.get('DEVELOPMENT', False)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,14 +28,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'is+jtuem4j3*uq6hd$7fvh!iy1q@wyo$p9f5^udi^6o*(8hj71')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = development
 
-# if development:
-#     ALLOWED_HOSTS = ['localhost']
-# else:
-#     ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
-
-ALLOWED_HOSTS = [os.environ.get('HEROKU_USERNAME')]
+if development:
+    ALLOWED_HOSTS = ['localhost']
+else:
+    ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
@@ -84,21 +82,17 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# if development:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': BASE_DIR / 'db.sqlite3',
-#         }
-#     }
-# else:
-#     DATABASES = {
-#         'default': dj_database_url.parse('postgres://vcvenakf:gATMhSQve9DaIheAVycak6AqOq_PcMhx@mel.db.elephantsql.com/vcvenakf')
-#     }
-
-DATABASES = {
-         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-     }
+if development:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
 
 
 # Password validation
